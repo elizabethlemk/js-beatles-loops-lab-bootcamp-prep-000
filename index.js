7b278fd553388db779c1dd497fdb3c8fe3a50a3f
ELIZABETH LE <elizabethlemk@yahoo.com>
2018-11-01 05:37:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments){
+  let array = []
+  for (let i = 0; i < musicians.length ; i++){array.push( musicians[i] + " plays " + instruments[i] )}
+  return array
+}
+
+function johnLennonFacts(facts){
+  let i = 0
+  while(i < facts.length){facts[i] = facts[i] + "!!!" ; i++}
+  return facts
+}
+
+function iLoveTheBeatles(n){
+  
+}
